2bf3e568a049729b6ac4f2bbd1a5f8185904ada4
funanani-ernest <funamazuwo@gmail.com>
2023-08-15 10:53:09 +0200

    AAdding pplication Slider

---
+++ G4L.UserManagement.BL/Interfaces/IAttendanceService.cs
@@ -0,0 +1,21 @@
+﻿using G4L.UserManagement.BL.Entities;
+using G4L.UserManagement.BL.Models;
+using G4L.UserManagement.BL.Models.Request;
+using G4L.UserManagement.BL.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G4L.UserManagement.BL.Interfaces
+{
+    public interface IAttendanceService
+    {
+        Task<List<AttendanceResponse>> GetAttendanceRegisterAsync(Guid userId);
+        Task<IEnumerable<Attendance>> GetPagedAttendancesAsync(int skip, int take);
+        Task<AttendanceResponse> SigningAttendanceRegisterAsync(Guid userId);
+        Task UpdateAttendanceGoalsAsync(AttendanceRequest attendanceRequest);
+        Task UpdateAttendanceRegisterAsync(AttendanceRequest attendanceRequest);
+    }
+}
